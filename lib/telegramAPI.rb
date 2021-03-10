fb62dd5b48b0c41169ab69fc89069969ab4705b2
Filippo Ragazzo <filipporagazzo89@gmail.com>
2021-03-10 14:21:24 +0100

    Get rid of obsolete URI::encode calls in favor of URI::encode_www_form

--- lib/telegramAPI.rb
+++ lib/telegramAPI.rb
@@ -19,7 +19,7 @@ class TelegramAPI
   def parse_hash hash
     ret = {}
     hash.map do |k,v|
-      ret[k]=URI::encode(v.to_s.gsub("\\\"", "\""))
+      ret[k]=URI::encode_www_form([v.to_s.gsub("\\\"", "\"")])
     end
     return ret
   end
@@ -35,7 +35,7 @@ class TelegramAPI
   end
   
   def setWebhook url
-    self.query("setWebhook", {"url"=>URI::encode(url)})
+    self.query("setWebhook", {"url"=>URI::encode_www_form([url])})
   end
 
   def getUpdates options={"timeout"=>0, "limit"=>100}
